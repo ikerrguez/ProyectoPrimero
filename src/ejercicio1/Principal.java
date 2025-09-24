43fede05348755e240fafd7a062e18a40703bf54
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-09-24 10:16:07 +0200

    Estado inicial de mi proyecto

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -2,6 +2,9 @@ package ejercicio1;
 
 public class Principal {
     public static void main() {
-
+        String nombre="Iker";
+        System.out.println("Mi nombre es " + nombre);
+        int edad=17;
+        System.out.println("Mi edad es de " +edad+" años");
     }
 }
