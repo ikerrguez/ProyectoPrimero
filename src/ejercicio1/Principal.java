76c147a7f6ebb057bdf272b8d85b0dc90a87a84c
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-09-25 13:06:03 +0200

    Ejercicio 1 acabado, pendiente de modificar disvisión

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,10 +1,29 @@
 package ejercicio1;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Principal {
-    public static void main() {
-        String nombre="Iker";
-        System.out.println("Mi nombre es " + nombre);
-        int edad=17;
-        System.out.println("Mi edad es de " +edad+" años");
+    public void main() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el primer número entero: ");
+        int numero1 = teclado.nextInt();
+        System.out.println("Ingrese el segundo número entero: ");
+        int numero2 = teclado.nextInt();
+
+        int resultadosuma= numero1+numero2;
+        System.out.println("El resultado de la suma es: "+resultadosuma);
+
+        int resultadoresta=  numero1-numero2;
+        System.out.println("El resultado de la resta es: "+resultadoresta);
+
+        int resultadomultiplicacion= numero1*numero2;
+        System.out.println("El resultado de la multiplicación es: "+resultadomultiplicacion);
+
+        int resultadodivision= numero1/numero2;
+        System.out.println("El resultado de la división es: "+resultadodivision);
+
     }
+
 }
+
