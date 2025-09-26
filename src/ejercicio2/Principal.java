3191eb9e2be5108c2753c8f34b528ea9398468b4
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-09-26 10:35:04 +0200

    Ejercicio 2 acabado

--- src/ejercicio2/Principal.java
+++ src/ejercicio2/Principal.java
@@ -1,7 +1,22 @@
 package ejercicio2;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¡Bienvenidos a mi calculadora de triángulos!");
+        //declaración variable
+        int base;
+        //asignación de un valor a una variable
+        System.out.println("Ingrese el numero base (entero): ");
+        base = sc.nextInt();
+        //podemos hacer declaración y asignación en una línea
+        System.out.println("Ingrese el numero altura (entero): ");
+        int altura = sc.nextInt();
+        //como hay una división, declaramos el resultado como decimal
+        double area = (double) (base+altura)/2;
+        System.out.println("El área del triángulo es: "+area);
 
     }
 }
