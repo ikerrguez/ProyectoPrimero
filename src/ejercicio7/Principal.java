1c214577907abd0511c89da553240ce3dae0e145
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-09-29 12:25:33 +0200

    Ejercicio 7 sin acabar, aún empezándolo

---
+++ src/ejercicio7/Principal.java
@@ -0,0 +1,14 @@
+package ejercicio7;
+
+import java.util.Scanner;
+
+public class Principal {
+    static void main() {
+        System.out.println("Programa iniciado");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número del 0 al 10");
+        int num = sc.nextInt();
+
+    }
+
+}
