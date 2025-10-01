2af0286f2cd5485ddfb5e8b90fef86e0af983d2a
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-10-01 09:45:52 +0200

    Ejercicio salidaNoche sin acabar, pendiente de modificar

--- src/ejerciciosalidanoche/Principal.java
+++ src/ejerciciosalidanoche/Principal.java
@@ -5,33 +5,48 @@ import java.util.Scanner;
 public class Principal {
     static void main() {
         System.out.println("Vamos a tomar el aire. Bienvenido");
-        double nivelAlcohol= 0.0;
-        double precioChupito= 3;
-        double precioCaña= 1.8;
-        double precioCombinado= 8;
-        double nivelChupito= 0.25;
-        double nivelCaña= 0.1;
-        double nivelCombinado= 0.4;
-        System.out.println("Dime tu nombre");
+        double nivelAlcohol = 0.0;
+        double precioChupito = 3;
+        double precioCanha = 1.8;
+        double precioCombinado = 8;
+        double nivelChupito = 0.25;
+        double nivelCanha = 0.1;
+        double nivelCombinado = 0.4;
         Scanner sc = new Scanner(System.in);
-        String nombreCliente=sc.nextLine();
-        System.out.println("Dime tu edad");
-        int edadCliente=sc.nextInt();
-        System.out.println("Dime cuánto dinero tienes");
-        int dineroCliente=sc.nextInt();
-        System.out.println(nombreCliente + " tiene " + edadCliente + " años y " + dineroCliente+ " euros ");
-        System.out.println("Buenas noches. Si quieres entrar en la discoteca debes ser mayor de edad.");
+        System.out.println("Ingrese el nombre ");
+        String nombreCliente = sc.nextLine();
+        System.out.println("Ingrese la edad ");
+        int edadCliente = sc.nextInt();
+        System.out.println("Ingrese el dinero del cliente ");
+        double dineroCliente = sc.nextDouble();
+        System.out.println("El cliente de nombre: " + nombreCliente + " con edad " + edadCliente + " años, tiene " + dineroCliente + " euros ");
+        System.out.println("¡BIENVENIDO A PELÍCANO!");
+        if (edadCliente >= 18) {
+            System.out.println("El portero comprueba tu estado");
+            if (nivelAlcohol >= 1.2) {
+                System.out.println("No entras, vas muy borracho");
+            } else {
+                System.out.println("Pase usted");
+                System.out.println("El cliente va a beber una canha");
+                if (dineroCliente >= precioCanha) {
+                    System.out.println("El cliente va a beber una canha");
+                    nivelAlcohol = nivelAlcohol + nivelCanha;
+                    dineroCliente = dineroCliente - precioCanha;
+                    System.out.println("El cliente solicita beber un chupito");
+                    if (dineroCliente >= precioChupito) {
+                        System.out.println("El cliente bebe un chupito");
+                        nivelAlcohol = nivelAlcohol + nivelChupito;
+                        dineroCliente = dineroCliente - precioChupito;
+                    }
+                }else 
+                    System.out.println("No tienes dinero suficiente para consumir");
+            }
+
+        } else {
+            System.out.println("Acceso denegado");
+        }
+    }}
 
 
-        if (18>edadCliente) {
-            System.out.println("No puedes entrar en la discoetca, vete a casa");
-        }else if (18<=edadCliente) {
-            System.out.println("Tienes el paso permitido a la discoteca");
-
-            System.out.println("Me voy a tomar una caña");
 
 
-
-
-    }
-}}
